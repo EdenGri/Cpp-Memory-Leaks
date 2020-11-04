f841f94f04e49fc08393582919800e80920308b4
EdenGri <eden7045@gmail.com>
2020-11-04 22:52:53 +0200

    add fields add isOccupiedNode() add occupyNode() add disconnectNode

--- headers/Graph.h
+++ headers/Graph.h
@@ -1,6 +1,7 @@
 #ifndef GRAPH_H_
 #define GRAPH_H_
 
+#include <set>
 #include <vector>
 
 class Graph{
@@ -13,8 +14,22 @@ public:
 
     // Returns: True if the node is infected, else return false.
     bool isInfected(int nodeInd);
+
+    // Adds the given node index to the occupies list.
+    void occupyNode(int nodeInd);
+
+    // Returns: True if the node is occupied, else return false.
+    bool isOccupies(int nodeInd);
+
+    // Disconnects node from the graph. remove the edges that contains the given node.
+    void disconnectNode(int nodeInd);
+
 private:
     std::vector<std::vector<int>> edges;
+    std::set<int> infectedList;
+    std::set<int> occupiesList;
+
+
 };
 
 #endif
--- src/Graph.cpp
+++ src/Graph.cpp
@@ -2,17 +2,38 @@
 // Created by spl211 on 31/10/2020.
 //
 
-#include <Graph.h>
+#include "Graph.h"
 
-Graph::Graph(std::vector<std::vector<int>> matrix) {
 
-}
+
+Graph::Graph(std::vector<std::vector<int>> matrix) : edges(matrix) , infectedList(), occupiesList(){}
+
 
 void Graph::infectNode(int nodeInd) {
-
+    infectedList.insert(nodeInd);
 }
-
 bool Graph::isInfected(int nodeInd) {
-    return false;
+    // just in set c++20 theres contains function.
+    return infectedList.find(nodeInd) != infectedList.end();
 }
+
+void Graph::occupyNode(int nodeInd) {
+    occupiesList.insert(nodeInd);
+}
+
+bool Graph::isOccupies(int nodeInd) {
+    // just in set c++20 theres contains function.
+    return occupiesList.find(nodeInd) != occupiesList.end();
+}
+
+void Graph::disconnectNode(int nodeInd) {
+    for (int i = 0; i <edges.size() ; ++i) {
+        edges[nodeInd][i]=0;
+        edges[i][nodeInd]=0;
+    }
+}
+
+
+
+
 

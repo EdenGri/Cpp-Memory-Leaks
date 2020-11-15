ecc2dc852f4123dbe9b0bc2fdc1586c9e9e7fe16
ofrypeler <ofrypeler4@gmail.com>
2020-11-15 23:34:42 +0200

    comment updates

--- src/Agent.cpp
+++ src/Agent.cpp
@@ -17,11 +17,13 @@ Agent* ContactTracer::clone() const {
 
 void ContactTracer::act(Session &session) {
     std::queue<int>& infectionQueue = session.getInfectionQueue();
+    //dequeues infected node from queue
     if (!infectionQueue.empty()) {
         int infectedNode = infectionQueue.front();
         infectionQueue.pop();
         Graph& g = session.getGraph();
         Tree* tree = g.Bfs(session, infectedNode);
+        //finds next infected node and disconnects from graph
         int deadly = tree->traceTree();
         g.disconnectNode(deadly);
         delete tree;
@@ -38,11 +40,12 @@ Agent* Virus::clone() const {
 
 void Virus::act(Session &session) {
     Graph& g = session.getGraph();
+    //infects node if not already infected
     if (!g.isInfected(nodeInd)) {
         g.infectNode(nodeInd);
         session.enqueueInfected(nodeInd);
     }
-
+    //spreads into uninfected neighbor nodes
     int healthyNeighbor = g.healthyNeighbor(nodeInd);
     if (healthyNeighbor != -1) {
         g.occupyNode(healthyNeighbor);
